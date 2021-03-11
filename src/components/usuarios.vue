8941c84cd7c4fedfb7ad70dff095a44114c8bb4f
Ernesto Diaz <ernesto.adp1@gmail.com>
2021-03-10 21:48:16 -0300

    arreglo tipo  dato number id api

--- src/components/usuarios.vue
+++ src/components/usuarios.vue
@@ -13,7 +13,7 @@
     export default {
         name:"usuarios",
         props:{
-                id:String,
+                id:{type:Number},
                 nombre:String,
                 email:String,
                 telefono:String
